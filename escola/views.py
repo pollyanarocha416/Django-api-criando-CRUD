ea29d758405541478f3326ba48e16a332dadcd84
pollyanarocha416 <pollyanarocha416@gmail.com>
2023-05-23 11:00:11 -0400

    api com uma tela

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,14 @@
 from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        alunos = [{'id':1, 'nome':'Guilherme'},
-        {'id': 2, 'nome': 'Polly'}]
-        
-        return JsonResponse(alunos, safe=False)
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
